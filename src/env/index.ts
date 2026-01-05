3062bcc9b6bbe579577c5b6912bdfdac0d6ccf82
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2026-01-05 08:26:10 -0300

    chore: setup deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite']),
   API_KEY: z.string(),
   PORT: z.coerce.number().default(3333),
 })
