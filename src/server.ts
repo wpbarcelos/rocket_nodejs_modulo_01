ee41ca439d8739722ea969c86f1c561851b0b4cc
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2026-01-05 10:15:13 -0300

    fix: binding server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+  console.log('HTTP server running on http://0.0.0.0:' + env.PORT)
 })
